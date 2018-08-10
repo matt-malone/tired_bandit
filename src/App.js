655d527595e6618f52bf166da82a4194e1e81050
Matthew Malone <mattmalone@thomsonreuters.com>
2018-08-10 15:44:58 -0500

    Getting fade-in to work

--- src/App.js
+++ src/App.js
@@ -6,21 +6,16 @@ import './css/font-awesome.min.css'
 import BanditSection from './BanditSection.js'
 import BrowserDetection from 'detect-browser'
 import ReactDOM from 'react-dom'
+import LazyLoad from 'react-lazyload'
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {};
-      this.setState(prevState =>({
-        scrolling: false;
-      }));
+
   }
   componentDidMount(){
-    window.addEventListener('onscroll',() => {
-      this.setState(prevState =>({
-        scrolling: true;
-      }));
-    });
+
   }
 
   render() {
@@ -28,43 +23,48 @@ class App extends Component {
 
     return (
       <div className="App">
-        <BanditSection
-          banditSectionContent={
-            <div><h1>{browser.name + '-' + browser.version}</h1>
-            <p className="major">A (modular, highly tweakable) responsive one-page template designed by <a href="https://html5up.net">HTML5 UP</a> and released for free under the <a href="https://html5up.net/license">Creative Commons</a>.</p>
-            <ul className="actions stacked">
-              <li><a href="#first" className="button big wide smooth-scroll-middle">Get Started</a></li>
-            </ul>
-          </div>
-        }
-        sectionType='banner'
-        sectionOrientation='orient-left'
-        sectionAlignment='content-align-left'
-        imagePosition='image-position-left'
-        screenFill='fullscreen'
-        fadeStyle='onload-image-fade-in onload-content-fade-right'
-        isScrolling={this.state.scrolling}
-        >
-        </BanditSection>
-        <BanditSection
-          banditSectionContent={
-            <div>
-							<h2>Magna etiam feugiat</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.</p>
-							<ul className="actions stacked">
-								<li><a href="#learnMore" className="button">Learn More</a></li>
-							</ul>
+        <LazyLoad>
+          <BanditSection
+            banditSectionContent={
+              <div><h1>{browser.name + '-' + browser.version}</h1>
+              <p className="major">A (modular, highly tweakable) responsive one-page template designed by <a href="https://html5up.net">HTML5 UP</a> and released for free under the <a href="https://html5up.net/license">Creative Commons</a>.</p>
+              <ul className="actions stacked">
+                <li><a href="#first" className="button big wide smooth-scroll-middle">Get Started</a></li>
+              </ul>
             </div>
-        }
-        sectionType='spotlight'
-        sectionIteration='02'
-        sectionOrientation='orient-left'
-        sectionAlignment='content-align-left'
-        screenFill=''
-        imagePosition='image-position-center'
-        fadeStyle='onscroll-image-fade-in'
-        >
-        </BanditSection>
+          }
+          sectionType='banner'
+          sectionOrientation='orient-left'
+          sectionAlignment='content-align-left'
+          imagePosition='image-position-left'
+          screenFill='fullscreen'
+          fadeStyle='onload-image-fade-in onload-content-fade-right'
+          isScrolling={this.state.scrolling}
+
+          >
+          </BanditSection>
+        </LazyLoad>
+        <LazyLoad>
+          <BanditSection
+            banditSectionContent={
+              <div>
+  							<h2>Magna etiam feugiat</h2>
+  							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.</p>
+  							<ul className="actions stacked">
+  								<li><a href="#learnMore" className="button">Learn More</a></li>
+  							</ul>
+              </div>
+          }
+          sectionType='spotlight'
+          sectionIteration='02'
+          sectionOrientation='orient-left'
+          sectionAlignment='content-align-left'
+          screenFill=''
+          imagePosition='image-position-center'
+          fadeStyle='onscroll-image-fade-in'
+          >
+          </BanditSection>
+        </LazyLoad>
         {
           // <header className="App-header">
         //   <img src={logo} className="App-logo" alt="logo" />
--- src/BanditSection.js
+++ src/BanditSection.js
@@ -5,30 +5,10 @@ import './css/noscript.css'
 import './css/font-awesome.min.css'
 
 class BanditSection extends Component {
-  isInViewPort(){
-    var myDOM = ReactDOM.findDOMNode(this);
-    var rect = myDOM.getBoundingClientRect();
-    var elementTop = rect.offsetTop;
-    var elementBottom = elementTop + rect.height;
-    var viewportTop = myDOM.scrollTop;
-    var viewportBottom = viewportTop + window.height;
-    return elementBottom > viewportTop && elementTop < viewportBottom;
-  }
   componentDidMount(){
 
-    this.interval = setInterval(() => {
-      if ( this.state.scrolling ) {
-        this.setState({
-          scrolling: false
-        });
-        if(isInViewPort()){
-
-        }
-      }
-    }, 250 );
   }
   componentWillUnmount(){
-    clearInterval(this.interval);
   }
   render() {
     return (
